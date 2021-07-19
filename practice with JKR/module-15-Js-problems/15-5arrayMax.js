a249c6389352eb169184cba5df9b74fc7cb79161
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-07-19 23:18:55 +0600

    some updated code

--- practice with JKR/module-15-Js-problems/15-5arrayMax.js
+++ practice with JKR/module-15-Js-problems/15-5arrayMax.js
@@ -1,11 +1,57 @@
-var marks = [12, 42, 54, 56, 66, 775, 454, 64];
+// var marks = [12, 42, 54, 56, 66, 775, 454, 64];
+// var max = marks[0];
+
+// for(i = 0; i < marks.length; i ++){
+//     var element = marks[i];
+//     if(element > max){
+//         max = element;
+//     }
+// }
+// console.log("the largest num is", max);
+
+var marks = [32, 53, 64, 22, 4, 54, 342, 56, 75];
 var max = marks[0];
 
-for(i = 0; i < marks.length; i ++){
+for (var i = 0; i < marks.length; i++){
     var element = marks[i];
-    if(element > max){
+    if( element > max){
         max = element;
     }
 }
+// console.log(max)
 
-console.log("the largest num is", max);
+
+// function megaFriend(friendsName){
+// var largestName = friendsName[0];
+//     for(var i = 0; i < friendsName.length; i++){
+//         var element = friendsName[i];
+//         if(element.length > largestName.length){
+//             largestName = element
+//         }
+//     }
+//     return largestName;
+// }
+
+
+// var result = megaFriend(["a", "ab", "abc", "abcd", "ABCDEF", "abcde"]);
+// console.log(result);
+
+function megaFriend(array) {
+    var bigName = array[0];
+    for (var i = 0; i < array.length; i++) {
+        var element = array[i];
+        if (bigName.length < element.length) {
+            bigName = element;
+        }
+    }
+    if (bigName == undefined) {
+        return 'please fill the box';
+    }
+    else if (typeof bigName == 'number') {
+        return 'The valu cannot be number';
+    }
+        return bigName;
+}
+
+var result = megaFriend(4);
+ console.log(result);
