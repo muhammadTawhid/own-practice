80c688ecb7d94e49bf79b4f704c0a949c68e773d
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-08-18 14:27:39 +0600

    some-updated

--- assignments/4Assignment/script.js
+++ assignments/4Assignment/script.js
@@ -2,8 +2,16 @@ document.getElementById("first-plus").addEventListener("click", function(){
     const firstInput = document.getElementById("first-input");
     const firstInputNumber = parseFloat(firstInput.value);
     const newInputValue = firstInputNumber + 1;
-    document.getElementById("first-input").innerText = newInputValue;
-    console.log('newInputValue', newInputValue);
+    document.getElementById("first-input").value = newInputValue;
     const subtotalValue = newInputValue * 150;
     document.getElementById("subtotal").innerText = "$" + subtotalValue;
+})
+
+document.getElementById("first-minus").addEventListener("click", function(){
+    const firstInput = document.getElementById("first-input");
+    const firstInputNumber = parseInt(firstInput.value);
+    const newInputValue = firstInputNumber - 1;
+    document.getElementById("first-input").value = newInputValue;
+    const subtotal = newInputValue * 150;
+    document.getElementById("subtotal").innerText = subtotal;
 })
