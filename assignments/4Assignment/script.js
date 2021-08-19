87b201a5371c5c281f1177e6114169395844a4f3
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-08-19 13:22:44 +0600

    some updated

--- assignments/4Assignment/script.js
+++ assignments/4Assignment/script.js
@@ -1,17 +1,35 @@
-document.getElementById("first-plus").addEventListener("click", function(){
-    const firstInput = document.getElementById("first-input");
-    const firstInputNumber = parseFloat(firstInput.value);
-    const newInputValue = firstInputNumber + 1;
-    document.getElementById("first-input").value = newInputValue;
-    const subtotalValue = newInputValue * 150;
-    document.getElementById("subtotal").innerText = "$" + subtotalValue;
-})
+// document.getElementById("first-plus").addEventListener("click", function(){
+//     // const firstInput = document.getElementById("first-input");
+//     // const firstInputNumber = parseInt(firstInput.value);
+//     // const newInputValue = firstInputNumber + 1;
+//     // document.getElementById("first-input").value = newInputValue;
+//     // const subtotal = newInputValue * 150;
+//     // document.getElementById("subtotal").innerText = "$" + subtotal;
+//     handlePlusMinus(true);
+// })
 
-document.getElementById("first-minus").addEventListener("click", function(){
-    const firstInput = document.getElementById("first-input");
+// document.getElementById("first-minus").addEventListener("click", function(){
+//     // const firstInput = document.getElementById("first-input");
+//     // const firstInputNumber = parseInt(firstInput.value);
+//     // const newInputValue = firstInputNumber - 1;
+//     // document.getElementById("first-input").value = newInputValue;
+//     // const subtotal = newInputValue * 150;
+//     // document.getElementById("subtotal").innerText = "$" + subtotal;
+//     handlePlusMinus(false); 
+// })
+
+function handlePlusMinus(isIncrease, product){
+    console.log(product);
+    const firstInput = document.getElementById(product + "-input");
     const firstInputNumber = parseInt(firstInput.value);
-    const newInputValue = firstInputNumber - 1;
-    document.getElementById("first-input").value = newInputValue;
+    newInputValue = firstInputNumber;
+    if(isIncrease == true){
+        newInputValue = firstInputNumber + 1;
+    }
+    if(isIncrease == false && 0 < newInputValue){
+        newInputValue = firstInputNumber - 1;
+    }
+    document.getElementById(product + "-input").value = newInputValue;
     const subtotal = newInputValue * 150;
-    document.getElementById("subtotal").innerText = subtotal;
-})
+    document.getElementById("subtotal").innerText = "$" + subtotal; 
+}
