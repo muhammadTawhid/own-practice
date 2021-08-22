cf607d393595dcf010ad3140de4039074c47980e
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-08-22 20:19:27 +0600

    pin-matched

--- practice/pin-matcher-master/script.js
+++ practice/pin-matcher-master/script.js
@@ -4,24 +4,16 @@ function handleGeneratePin(){
     document.getElementById("generatedNumber").value = generateRoundNumber;
 }
  
-// function putPin(){
-//     const numbers = document.querySelectorAll(".button");
-//     for(let i = 0; i < numbers.length; i++){
-//     const element = numbers[i];
-
-//     document.getElementById("givePin").value = element.innerHTML
-    
-//     console.log(element.innerHTML);
-//     }
-// }
-
-function putPin(number){
-    const newNumber = number.toString();
-
+function putPin(value){
+    const newNumber = value.toString();
     let beforeNumber = document.getElementById("givePin").value;
-
     const setNumber = beforeNumber + newNumber;
     document.getElementById("givePin").value = setNumber;
+    if(value == "clearAll"){
+        document.getElementById("givePin").value = "";
+        document.getElementById("successNotify").style.display = "none";
+        document.getElementById("wrongNotify").style.display = "none";
+    }
 }
 
 function handleNotify(){
@@ -29,13 +21,21 @@ function handleNotify(){
     const putNumber = document.getElementById("givePin").value;
     if(generatedNumber == putNumber){
         document.getElementById("successNotify").style.display = "block";
+        document.getElementById("wrongNotify").style.display = "none";
     }
-    
     else{
         document.getElementById("wrongNotify").style.display = "block";
-    }
-
-    if(document.getElementById("wrongNotify").style.display = "block"){
         document.getElementById("successNotify").style.display = "none";
+
     }
 }
+
+function handleRemoveChr(){
+    const removeChr = document.getElementById("givePin").value;
+    const removed = removeChr.slice(0, -1);
+    document.getElementById("givePin").value = removed;
+}
+
+// function clearInput(){
+//     document.getElementById("givePin").value = "";
+// }
