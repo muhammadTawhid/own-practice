85f92de84195449c6f5875d61560640c2a20e06c
muhammadtawhid <muhammadtawhid.me@gmail.com>
2021-09-07 02:00:06 +0600

    some updated

---
+++ practice/26_5-1hard-rock-master/script.js
@@ -0,0 +1,20 @@
+// const searchSong = () =>{
+//     const searchInput = document.getElementById("searchInput").value;
+//     url = (`https://theaudiodb.com/api/v1/json/1/mvid.php?i=112024`)
+//     fetch(url)
+//     .then(res => res.json())
+//     .then(data => showSongs(data.mvids))
+// }
+
+// const showSongs = data =>{
+//     data.forEach(song => {
+//         const songDetails = document.getElementById("songDetails");
+//         songDetails.innerHTML = `
+//         <img src="${song.strTrackThumb}"/>
+//         <h3>fd</h3>
+//         `
+//     });
+// }
+
+
+//-----------------------------------------i tried but the api doesn't exist  b 
